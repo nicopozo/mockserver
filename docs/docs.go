47461af127f606b8013744ec670e99ff49a8506e
nicopozo-peya <nicolas.pozo@pedidosya.com>
2022-08-25 21:11:23 -0300

    swagger for version 2.0.0

--- docs/docs.go
+++ docs/docs.go
@@ -533,7 +533,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "1.0",
+	Version:          "2.0.0",
 	Host:             "localhost:8080",
 	BasePath:         "/mock-server",
 	Schemes:          []string{"http"},
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd ..
+
 docker build -t nicopozo/mock-service:2.0.0 .
 docker build -t nicopozo/mock-service:latest .
 docker push nicopozo/mock-service:2.0.0
